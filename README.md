ac461cb34138dc0902344e98464e43ab92dd1c24
sreedev <sreedevpadmakumar@gmail.com>
2021-12-23 09:00:09 +0530

    recursion

---
+++ README.md
@@ -0,0 +1,7 @@
+# Yet Another Brainfuck Interpreter
+
+This is yet another implementation of the Brainfuck Interpreter. This time in 45 Lines of Ruby.
+A functional implementation of the Brainfuck interpreter written from Scratch in less than 45 lines of Ruby.
+
+
+## About This 
--- brainfuck.rb
+++ brainfuck.rb
@@ -1,49 +1,45 @@
+#!/usr/bin/ruby
 # frozen_string:literal: true
 
 module Brainfuck
   class VM
     def ops
-      @ops ||= {
-        ">" => ->(state) { state.merge({data_p: state[:data_p]+1, inst_p: state[:inst_p] + 1}) },
-        "<" => ->(state) { state.merge({data_p: state[:data_p]-1, inst_p: state[:inst_p] + 1}) },
-        "+" => ->(state) { state.merge({memory: state[:memory].merge(state[:data_p] => (state[:memory][state[:data_p]]+1 % 127).abs), inst_p: state[:inst_p] + 1}) },
-        "-" => ->(state) { state.merge({memory: state[:memory].merge(state[:data_p] => (state[:memory][state[:data_p]]-1 % 127).abs), inst_p: state[:inst_p] + 1}) },
-        "." => ->(state) { print(state[:memory][state[:data_p]].chr); state.merge({inst_p: state[:inst_p] + 1}) },
-        "," => ->(state) { state.merge({memory: state[:memory].merge({ state[:data_p] => STDIN.getch.bytes[0] }), inst_p: state[:inst_p] + 1}) },
-        "[" => ->(state) { 
+      @ops ||= (Hash.new { ->(state) { state.merge({inst_p: state[:inst_p] + 1}) } }).merge({
+        ?> => ->(state) { state.merge({data_p: state[:data_p]+1, inst_p: state[:inst_p] + 1}) },
+        ?< => ->(state) { state.merge({data_p: state[:data_p]-1, inst_p: state[:inst_p] + 1}) },
+        ?+ => ->(state) { state.merge({memory: state[:memory].merge(state[:data_p] => (state[:memory][state[:data_p]]+1 % 127).abs), inst_p: state[:inst_p] + 1}) },
+        ?- => ->(state) { state.merge({memory: state[:memory].merge(state[:data_p] => (state[:memory][state[:data_p]]-1 % 127).abs), inst_p: state[:inst_p] + 1}) },
+        ?. => ->(state) { print(state[:memory][state[:data_p]].chr); state.merge({inst_p: state[:inst_p] + 1}) },
+        ?, => ->(state) { state.merge({memory: state[:memory].merge({ state[:data_p] => STDIN.getch.bytes[0] }), inst_p: state[:inst_p] + 1}) },
+        ?[ => ->(state) { 
           state[:stack].push(state[:inst_p]) && ((
             state[:memory][state[:data_p]].zero? && state.merge(
               inst_p: state[:program]
               .slice(state[:inst_p]..-1)
-              .filter_map
-              .with_index { |el, index| index + 1 if el == "]" }[state[:stack].size]
+              .filter_map.with_index { |el, index| index + 1 if el == "]" }[state[:stack].size]
             )
           ) || state.merge({inst_p: state[:inst_p] + 1}))
         },
-        "]" => ->(state) {
+        ?] => ->(state) {
           (matching_paren=state[:stack].pop) && ((
             !state[:memory][state[:data_p]].zero? && 
             state.merge({inst_p: matching_paren})
           ) || state.merge({inst_p: state[:inst_p] + 1}))
-        }
-      }
+        },
+        ?# => ->(state) { puts(debug(state)) || state.merge({inst_p: state[:inst_p] + 1}) }
+      })
     end
 
     def exec(source)
-      machine = {
-        memory: Hash.new { 0 },
-        inst_p: 0,
-        data_p: 0,
-        stack: [],
-        program: source.strip.chars
-      }
+      run({ memory: Hash.new { 0 }, inst_p: 0, data_p: 0, stack: [], program: source.strip.chars })
+    end
 
-      loop { (machine = ops[machine[:program][machine[:inst_p]]].(machine)) && (machine[:inst_p] >= machine[:program].length && break) }
-      machine
+    def run(machine)
+      return (machine[:inst_p] >= machine[:program].length ? machine : run(ops[machine[:program][machine[:inst_p]]].(machine)))
     end
 
     def debug(machine)
-      { m: machine.slice(:memory, :data_p, :inst_p), in: machine[:program][machine[:inst_p]] }
+      { m: machine.slice(:memory, :data_p, :inst_p), in: machine[:program][machine[:inst_p]], stack: machine[:stack] }
     end
   end
 end
